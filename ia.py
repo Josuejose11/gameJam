f2b5052c6e3399e2e942a778e0cc44560c8bc88a
Josuejose11 <josuejose11@outlook.com>
2026-08-17 20:34:47 -0300

    Update ia.py

--- ia.py
+++ ia.py
@@ -3,6 +3,7 @@ from google import genai
 
 client = genai.Client(api_key="Sua_API_KEY_Aqui")
 
+response = client.models.generate_content(
     model="gemini-3.6-flash",
     contents="Olá! Responda apenas: Funcionou!"
 )
