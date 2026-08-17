d7c8cf9b9fc6d99944feda20bc2732478b38a52c
Josuejose11 <josuejose11@outlook.com>
2026-08-17 16:26:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 print("Seja bem vindo ao nosso sistema!")
+print ("Somos um sistema que te ajuda a organizar sua empresa tendo em mente a sustentabilidade")
 
-print("Seja bem vindo ao nosso sistema de suporte!")
-
-print("Fique por dentro das nossas informações aqui.\n")
-resposta = input("Deseja saber mais sobre nossa empresa? (S/N): ")
-if resposta == "S":
-    print("Veja abaixo as informações") conexao_informaçoes
-    
-elif resposta == "N":
-    print("Veja aqui outras opções para você")
-    
-if resposta not in ["S", "N"]:
-    print("Digite uma opção válida")
-    
+while True:
+    print("Selecione o que você gostaria de fazer:")
+    escolha = input(" | 0 - Sair \n | 1 - Informações sobre a sustentabiidade \n | 2 - Solução de problemas")
