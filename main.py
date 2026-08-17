e42ecd51c09b45ddb3e6337e9473bdffd71c622c
Josuejose11 <josuejose11@outlook.com>
2026-08-17 16:32:46 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ print("Seja bem vindo ao nosso sistema!")
 print ("Somos um sistema que te ajuda a organizar sua empresa tendo em mente a sustentabilidade")
 
 while True:
-    print("Selecione o que você gostaria de fazer:")
-    escolha = input(" | 0 - Sair \n | 1 - Informações sobre a sustentabiidade \n | 2 - Solução de problemas")
+    menu()
---
+++ menu.py
@@ -0,0 +1,3 @@
+def menu():
+    print("Selecione o que você gostaria de fazer:")
+    escolha = input(" | 0 - Sair \n | 1 - Informações sobre a sustentabiidade \n | 2 - Solução de problemas")
