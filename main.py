f5c992b0924856c6e41175bf62de9966419e9f77
GugaPixel360 <gmadureira198@gmail.com>
2026-08-17 16:13:54 -0300

    Enhance welcome messages and user input handling

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("Seja bem vindo ao nosso sistema!")
+
+print("Seja bem vindo ao nosso sistema de suporte!")
+
+print("Fique por dentro das nossas informações aqui.\n")
+resposta = input("Deseja saber mais sobre nossa empresa? (S/N): ")
+if resposta == "S":
+    print("Veja abaixo as informações") conexao_informaçoes
+    
+elif resposta == "N":
+    print("Veja aqui outras opções para você")
+    
+if resposta not in ["S", "N"]:
+    print("Digite uma opção válida")
+    
