b9aebf8de663c7197583081e42a19a60f4e49f9b
Josuejose11 <josuejose11@outlook.com>
2026-08-18 09:09:52 -0300

    atualizacao da IA

--- ia.py
+++ ia.py
@@ -1,23 +1,52 @@
 from google import genai
 from google.genai import types
+import time
+import threading
 
-import warnings
+#def de "Pensando..." 
+def carregando():
+    while pensando:
+        print("\rPensando   ", end="", flush=True)
+        time.sleep(0.5)
 
-warnings.filterwarnings(
-    "ignore",
-    message="Direct use of automatic function calling*"
-)
+        print("\rPensando.  ", end="", flush=True)
+        time.sleep(0.5)
 
+        print("\rPensando.. ", end="", flush=True)
+        time.sleep(0.5)
 
+        print("\rPensando...", end="", flush=True)
+        time.sleep(0.5)
+
+# Def da conexao e cvc c o gemini
 def gemini(texto):
-    client = genai.Client(api_key="SUA_API_AQUI")
+    client = genai.Client(api_key="sua_chave_aqui")
+
+    config = types.GenerateContentConfig(
+        automatic_function_calling=types.AutomaticFunctionCallingConfig(
+            disable=True
+        )
+    )
 
     response = client.models.generate_content(
         model="gemini-3.6-flash",
-        contents=texto
+        contents="Chat: "+texto,
+        config=config
     )
+
     return response.text
 
-msg = input("Digite uma mensagem para o gemini: ")
-print ("Pensando...")
-print(gemini("Me de apenas a resposta simples para: " + msg))
+
+
+pensando = True
+
+msg = input("Digite uma mensagem para o Gemini: ")
+
+thread = threading.Thread(target=carregando)
+thread.start()
+
+resposta = gemini("Me dê apenas uma resposta simples para: " + msg)
+
+pensando = False
+thread.join()
+
