d85011404e099dc1782c90e0c58a12ac9d2c9d26
Josuejose11 <josuejose11@outlook.com>
2026-08-19 11:02:43 -0300

    .

---
+++ sql.py
@@ -0,0 +1,38 @@
+from utilidades import *
+def sql():
+    conn = criar_conexao()
+
+    if conn is None:
+        print("Erro ao conectar com o banco!")
+        return False
+
+    cursor = conn.cursor()
+
+
+    cursor.execute("CREATE DATABASE IF NOT EXISTS Hakaton")
+    cursor.execute("USE Hakaton")
+    
+
+    # Criação da tabela de usuários
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255) NOT NULL,
+            senha VARCHAR(255) NOT NULL
+        )
+    """)
+
+    # Criação da tabela de conversas
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS conversas (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            id_usuario INT NOT NULL,
+            mensagem_usuario TEXT NOT NULL,
+            resposta_ia TEXT NOT NULL,
+            FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+        )
+    """)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
