bb87a4549164e9e78f4bf08e4c9bc14df67fd2ea
GugaPixel360 <gmadureira198@gmail.com>
2026-08-19 08:45:57 -0300

    prompts

--- prompts.py
+++ prompts.py
@@ -38,17 +38,32 @@ Bio5_3 = ...
 # ===========
 # ECONOMICA 
 # ===========
+
 E1_1 = "Analise a empresa na classificação Econômica, identificando o Aumento dos custos de produção. Sugira uma solução sustentável para reduzi-lo. "
 E1_2 = "Analise a empresa na classificação Econômica, identificando o Aumento dos custos de mão de obra. Sugira uma solução sustentável para reduzi-lo. "
 E1_3 = "Analise a empresa na classificação Econômica, identificando a Queda na produção. Sugira uma solução sustentável para reduzi-lo. "
 E1_4 = "Analise a empresa na classificação Econômica, identificando o Individamento Empresarial. Sugira uma solução sustentável para reduzi-lo. "
 
 E2_1 = "Analise a empresa na classificação Econômica, identificando o desemprego,identificando o Aumento do custo de mão de obra. Sugira uma solução sustentável para reduzi-lo. "
+E2_2 = "Analise a empresa na classificação Econômica, identificando o desemprego,identificando a Redução da demanda por trabalhadores. Sugira uma solução sustentável para reduzi-lo. "
+E2_3 = "Analise a empresa na classificação Econômica, identificando o desemprego,identificando a Substituição de trabalhadores por tecnologia. Sugira uma solução sustentável para reduzi-lo. "
 
+E3_1 = "Analise a empresa na classificação Econômica, identificando a inflacao,identificando o Aumento dos preços de bens e serviços. Sugira uma solução sustentável para reduzi-lo. "
+E3_2 = "Analise a empresa na classificação Econômica, identificando a inflacao,identificando o Aumento dos custos de produção. Sugira uma solução sustentável para reduzi-lo. "
+E3_3 = "Analise a empresa na classificação Econômica, identificando a inflacao,identificando a Perda de poder de compra da moeda. Sugira uma solução sustentável para reduzi-lo. "
+
+E4_1 = "Analise a empresa na classificação Econômica, identificando a dívida pública, e o Aumento do endividamento governamental. Sugira uma solução sustentável para reduzi-lo. "
+E4_2 = "Analise a empresa na classificação Econômica, identificando a dívida pública, e a Redução da receita fiscal. Sugira uma solução sustentável para reduzi-lo. "
+E4_3 = "Analise a empresa na classificação Econômica, identificando a dívida pública, e o Aumento dos juros da dívida. Sugira uma solução sustentável para reduzi-lo. "
+
+E5_1 = "Analise a empresa na classificação Econômica, identificando a instabilidade econômica, e o Flutuações significativas na economia. Sugira uma solução sustentável para reduzi-lo. "
+E5_2 = "Analise a empresa na classificação Econômica, identificando a instabilidade econômica, e os Impactos negativos no crescimento econômico. Sugira uma solução sustentável para reduzi-lo. "
+E5_3 = "Analise a empresa na classificação Econômica, identificando a instabilidade econômica, e o Efeito sobre o emprego. Sugira uma solução sustentável para reduzi-lo. "
 
 # ===========
 # BIOECONOMICA 
 # ===========
+
 BioE1_1 = "Analise a empresa na classificação Bioeconomia, identificando desperdício e uso excessivo de matéria-prima orgânica. Sugira uma solução sustentável para reduzi-lo. "
 BioE1_2 = "Analise a empresa na classificação Bioeconomia, identificando a falta de reaproveitamento de matéria-prima orgânica. Sugira uma solução sustentável para reaproveitar esses recursos."
 BioE1_3 = "Analise a empresa na classificação Bioeconomia, identificando a perda de matéria-prima orgânica durante a produção. Sugira uma solução sustentável para reaproveitar esses recursos."
